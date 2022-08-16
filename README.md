07e11d7e6f944752549b70b37d91ceae073ef9e3
ひろろ <hrsi.ad@gmail.com>
2022-08-16 15:42:58 +0900

    socket.io実装

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 # チャットアプリ
 https://www.youtube.com/watch?v=6KT9T73-Iik&t=64s
 
+https://github.com/Shin-sibainu/real-time-chatapp-using-socket.io
+
 ## セットアップ
 
 1. git init 
@@ -9,3 +11,11 @@ https://www.youtube.com/watch?v=6KT9T73-Iik&t=64s
 1. npm i express
 1. npm i socket.io
 2. npm i nodemon
+
+### 問題点
+
+1. cssファイルが認識されない。
+2. →Node.js 側で設定いるのか？
+
+    <script src="/socket.io/socket.io.js"></script>
+はどこを参照してるのか？
--- server.js
+++ server.js
@@ -6,10 +6,22 @@ const server = http.createServer(app);
 const io = require("socket.io")(server);
 const PORT = 3000;
 
+// app.use(express.static(__dirname + '/')
+
 server.listen(PORT, () => {
   console.log("server running !");
 });
 
+io.on("connection", (socket) => {
+  console.log("ユーザーが接続しました。");
+
+  socket.on("chat message", (msg) => {
+    console.log(msg)
+    io.emit("chat message", msg);
+  });
+});
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  console.log("dir:" + __dirname);
+  res.sendFile(__dirname + "/index.html");
 });
